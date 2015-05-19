9176678083746b16ee680af9309930938464b3ef
aserranocasla <aserranocasla@gmail.com>
2015-05-19 18:19:55 +0200

    Crear comentario

--- models/models.js
+++ models/models.js
@@ -1,4 +1,5 @@
 var path = require ('path');
+var pg = require ('pg');
 
 // Postgres DATABASE_URL = postgres://user:passwd@host:port/database
 // SQLite   DATABASE_URL = sqlite://:@:/
@@ -27,17 +28,29 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 );
 
 //Importar la definicion de la tabla Quiz en quiz.js
+var quiz_path = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 
+// Importar definicion de la tabla Comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; //exportar definición de tabla Quiz
+exports.Comment = Comment;
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
 	//success(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().then(function(cont){
-		if(count === 0){ // La tabla se inicializa solo si esta vacía
+		if(count === 0) { // La tabla se inicializa solo si esta vacía
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'
+						});
+			Quiz.create({ pregunta: 'Capital de Portugal',
+						  respuesta: 'Lisboa'
 						})
 			.then(function(){console.log('Base de datos inicializada')});
 
